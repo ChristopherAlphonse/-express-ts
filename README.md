a63a566d4bdd63370520a7daa4412c63b41d2dc9
Christopher Alphonse <christopheralphonse96@gmail.com>
2022-11-23 10:55:51 -0500

    second commit

--- README.md
+++ README.md
@@ -2,20 +2,43 @@
 
 A simple barebones boilerplate to use TypeScript with Node and Express.
 
-## Run the app
+## Download node_modules
 
-```
-npm run dev
-```
-
-## Build/Compile the app
-
-```
-npm run build
+```sh
+ yarn
 ```
 
-## Run the app (in production)
+## Updated node_modules
+
+```powershell
+ yarn devi
+ cls
+```
+
+## Create and Compile to dist folder
+
+```sh
+ yarn start
+```
+
+## Close terminal
+
+```sh
+CTRL + C
+
 
 ```
-npm start
+
+- Terminate batch job (Y/N)? Y
+
+## Start project
+
+```sh
+yarn dev
+```
+
+## Navigate to browser or whatever PORT is set in the .env file
+
+```sh
+localhost:8081
 ```
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,31 @@
-import express from 'express';
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/.env" });
+
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const allowedOrigins = ["http://localhost:5173/"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 const app = express();
 
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Home Page");
 });
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors(options));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const PORT = parseInt(process.env.PORT as string, 10) || 8081;
+
+app.listen(PORT, () => {
+  console.log(`App is running on 😇 ${PORT}`);
 });
